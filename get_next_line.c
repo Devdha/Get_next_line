725f49413f0750a18630a1b7b967a85e1cff7168
p0tt3r-iOS <france998811@gmail.com>
2021-12-08 20:50:12 +0900

    New gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,98 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 19:33:03 by dha               #+#    #+#             */
-/*   Updated: 2021/12/08 19:33:06 by dha              ###   ########seoul.kr  */
+/*   Updated: 2021/12/08 20:49:56 by dha              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *) malloc(sizeof(char) * (len + 1));
+	if (str == 0)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		if (i < ft_strlen(s1))
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - ft_strlen(s1)];
+		i++;
+	}
+	str[i] = '\0';
+	free(s1);
+	free(s2);
+	return (str);
+}
+
+static int	ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char) c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+static char	*get_line(char **backup, int fd)
+{
+	char	*line;
+	char	*new_backup;
+	int		i;
+
+	i = 0;
+	while (backup[fd][i])
+	{
+		if (backup[fd][i] == '\n')
+			break ;
+		i++;
+	}
+	line = ft_substr(backup[fd], 0, i + 1);
+	new_backup = ft_substr(backup[fd], i + 1, ft_strlen(backup[fd] - i));
+	free(backup[fd]);
+	backup[fd] = new_backup;
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*backup[OPEN_MAX];
+	char		*buffer;
+	char		*tmp;
+	int			len;
+
+	if (fd >= OPEN_MAX || fd < 0 || BUFFER_SIZE < 1)
+		return (0);
+	if (backup[fd] == 0)
+		backup[fd] = ft_strdup("");
+	if (backup[fd] == 0)
+		return (0);
+	if (ft_strchr(backup[fd], '\n'))
+		return (get_line(backup, fd));
+	buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buffer == 0)
+		return (0);
+	len = read(fd, buffer, BUFFER_SIZE);
+	buffer[len] = '\0';
+	if (len < 1)
+	{
+		free(buffer);
+		if (len == -1)
+			return (0);
+		return (get_line(backup, fd));
+	}
+	tmp = ft_strjoin(backup[fd], buffer);
+	if (tmp == 0)
+		return (0);
+	backup[fd] = tmp;
+	return (get_next_line(fd));
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,24 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 19:33:04 by dha               #+#    #+#             */
-/*   Updated: 2021/12/08 19:33:05 by dha              ###   ########seoul.kr  */
+/*   Updated: 2021/12/08 20:04:44 by dha              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 10240
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoinf(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif
